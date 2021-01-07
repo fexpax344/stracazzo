7c00e535c94cc51178ce60043d9e8c2b7b903a9e
Samuele Stasi <samuele.stasi2002@gmail.com>
2021-01-07 11:21:10 +0100

    creazione soluzione

---
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/SenzaTitolo1.cpp
@@ -0,0 +1,19 @@
+	case 0 : cout<<"::::::::::::::::::::: "<<endl;
+			         cout<<":0) nero;           : "<<endl;
+                     cout<<":1) blu;            : "<<endl;
+                     cout<<":2) verde;          : "<<endl;
+                     cout<<":3) verde acqua;    : "<<endl;
+                     cout<<":4) bordeaux;       : "<<endl;
+                     cout<<":5) viola;          : "<<endl;
+                     cout<<":6) verde oliva;    : "<<endl;
+                     cout<<":7) grigio chiaro;  : "<<endl; 
+                     cout<<":8) grigio;         : "<<endl;
+                     cout<<":9) blu;            : "<<endl;
+                     cout<<"::::::::::::::::::::: "<<endl;
+                     cout<<"immettere numero:"     <<endl;
+                     cin >>a;
+                     if (a=0){system("color 0f");
+					 }else{system ("color 1f");
+					 }
+                     
+				break;	

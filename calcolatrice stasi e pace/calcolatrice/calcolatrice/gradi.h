4ef760ee03e4a95952021858b594042945b1133e
fexpax344 <federicopace2004@gmail.com>
2021-01-25 17:44:39 +0100

    dre

--- calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.h
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.h
@@ -4,33 +4,36 @@ using namespace std;
 
 class gradi {
 public:
-	float r1, r2, fg, fs, fp, x1, x2;
+	float Rs, Rp, fg, fs, fp;
 
 	void Secondi(float s, float S) {
 		fs = s + S;
 		if (fs >= 60) {
 			fs = fs - 60;
-			x1 == 1;
+			Rs = 1;
 		}else{
-			x1 = 0;
+			Rs = 0;
 		}
 	}
 	void Primi(float p, float P) {
-		fp = p + P + x1;
-		if (fp >= 60) {
+		fp = p + P;
+		if (fp >= 59) {
 			fp = fp - 60;
-			x2 = 1;
+			Rp = 1;
 		}else{
-			x2 = 0;
+			Rp = 0;
+		}
+		if (Rs == 1) {
+			fp + 1;
 		}
 	}
 	void Gradi(float g, float G) {
-		fg = g + G + x2;
+		fg = g + G + Rp;
 	}
 	void print()
 	{
 	cout << " " << endl;
-    cout << "il risultato è: " << endl;
+    cout << "il risultato e': " << endl;
 	cout << "gradi: " << fg << endl;
 	cout << "primi: " << fp << endl;
 	cout << "secondi: " << fs << endl;

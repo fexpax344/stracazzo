2aea81ecb3ac926abd2c3a8044e0d5fdd693f162
fexpax344 <federicopace2004@gmail.com>
2021-01-25 13:21:35 +0100

    tre

---
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.cpp
@@ -0,0 +1,1 @@
+#include "gradi.h"
--- calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.h
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.h
@@ -4,31 +4,36 @@ using namespace std;
 
 class gradi {
 public:
-	float r1, r2, fg, fs, fp;
-	void Primi(float p, float P) {
-		fp = p + P;
-		if (fp >= 60) {
-			r1 = fp - 60;
-			fp == r1;
-			fs = fs + 1;
-		}
-	}
+	float r1, r2, fg, fs, fp, x1, x2;
+
 	void Secondi(float s, float S) {
 		fs = s + S;
-		if (fs <= 60) {
-			r2 = fs - 60;
-			fs == r2;
-			fg = fg + 1;
+		if (fs >= 60) {
+			fs = fs - 60;
+			x1 == 1;
+		}else{
+			x1 = 0;
+		}
+	}
+	void Primi(float p, float P) {
+		fp = p + P + x1;
+		if (fp >= 60) {
+			fp = fp - 60;
+			x2 = 1;
+		}else{
+			x2 = 0;
 		}
 	}
 	void Gradi(float g, float G) {
-		fg = g + G;
+		fg = g + G + x2;
 	}
 	void print()
 	{
-	cout << "La media e' " << fg << endl;
-	cout << "La media e' " << fp << endl;
-	cout << "La media e' " << fs << endl;
+	cout << " " << endl;
+    cout << "il risultato è: " << endl;
+	cout << "gradi: " << fg << endl;
+	cout << "primi: " << fp << endl;
+	cout << "secondi: " << fs << endl;
 	}
 
 };

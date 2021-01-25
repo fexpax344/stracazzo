54774c41a99d6d813e530faa75dc310fe8c66308
fexpax344 <federicopace2004@gmail.com>
2021-01-25 11:16:28 +0100

    gui

---
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/gradi.h
@@ -0,0 +1,30 @@
+#pragma once
+class gradi {
+public:
+	float r1, r2, fg, fs, fp;
+	void Primi(float p, float P) {
+		fp = p + P;
+		if (fp >= 60) {
+			r1 = fp - 60;
+			fp == r1;
+			fs = fs + 1;
+		}
+	}
+	void Secondi(float s, float S) {
+		fs = s + S;
+		if (fs <= 60) {
+			r2 = fs - 60;
+			fs == r2;
+			fg = fg + 1;
+		}
+	}
+	void Gradi(float g, float G) {
+		fg = g + G;
+	}
+	cout << "La media e' " << fg << endl;
+	cout << "La media e' " << fp << endl;
+	cout << "La media e' " << fs << endl;
+
+};
+
+

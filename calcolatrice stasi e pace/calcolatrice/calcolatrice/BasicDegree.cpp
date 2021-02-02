51525c434047d595ebb6259987389e72a39b5d95
fexpax344 <federicopace2004@gmail.com>
2021-02-02 14:08:39 +0100

    cuhb

--- calcolatrice stasi e pace/calcolatrice/calcolatrice/BasicDegree.cpp
+++ calcolatrice stasi e pace/calcolatrice/calcolatrice/BasicDegree.cpp
@@ -6,5 +6,28 @@ BasicDegree::BasicDegree(int degrees, int first, int seconds)
 
 BasicDegree& BasicDegree::operator+(BasicDegree & right)
 {
+	void Secondi(float s, float S) {
+		fs = s + S;
+		if (fs >= 60) {
+			fs -= 60;
+			Rs = 1;
+		}
+		else {
+			Rs = 0;
+		}
+	}
+	void Primi(float p, float P) {
+		fp = p + P + Rs;
+		if (fp >= 60) {
+			fp -= 60;
+			Rp = 1;
+		}
+		else {
+			Rp = 0;
+		}
+	}
+	void Gradi(float g, float G) {
+		fg = g + G + Rp;
+	}
 	return right;
 }
